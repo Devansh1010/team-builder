c3dfa8b1d3b506fef41a7f2174bcadee858465c4
Devansh1010 <devanshprajapati36@gmail.com>
2025-12-08 16:42:04 +0530

    Add theming to app

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -4,12 +4,13 @@ import DashboardCard from '@/components/DashboardCard'
 import { Button } from '@/components/ui/button'
 import axios from 'axios'
 import { Moon, Sun, User } from 'lucide-react'
+import { useTheme } from 'next-themes'
 
 import { useState } from 'react'
 import { toast } from "sonner"
 
 const page = () => {
-    const [theme, setTheme] = useState(true)
+    const { theme, setTheme } = useTheme()
 
     return (
         <div className='w-full px-10 py-6'>
@@ -21,14 +22,12 @@ const page = () => {
 
                 </div>
 
-                <div className="flex gap-3">
+                <div className="flex gap-3 ">
 
                     <Button
-                        variant="ghost"
-                        className="hover:bg-gray-100"
-                        onClick={() => setTheme((prev) => !prev)}
+                        onClick={() => setTheme(theme === "light" ? "dark" : "light")}
                     >
-                        {theme ? <Sun className="w-5 h-5 text-yellow-500" /> : <Moon className="w-5 h-5 text-blue-500" />}
+                        {theme === "light" ? <Moon /> : <Sun />}
                     </Button>
                 </div>
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css"
-
+import { ThemeProvider } from "@/components/theme-provider"
 
 // Sonar
 import { Toaster } from "@/components/ui/sonner"
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,14 +28,20 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
-    <html lang="en" className="dark">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ThemeProvider>
           <main className="w-full">
+
             {children}
+
+
           </main>
+        </ThemeProvider>
         <Toaster />
       </body>
     </html>
